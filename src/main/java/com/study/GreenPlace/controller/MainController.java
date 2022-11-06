3d9fbb801768e8c1152c56a12b56f0d410d839f5
lephuquan <80085752+lephuquan@users.noreply.github.com>
2022-11-06 08:32:43 +0700

    process cros

--- src/main/java/com/study/GreenPlace/controller/MainController.java
+++ src/main/java/com/study/GreenPlace/controller/MainController.java
@@ -9,11 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,6 +23,7 @@ public class MainController {
     private JwtTokenProvider tokenProvider;
 
     @PostMapping("/login")
+    @CrossOrigin(origins = "http://localhost:8080")
     public LoginResponse authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
         // Xác thực từ username và password.
