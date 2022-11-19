2bfad6cdf33baa9378af8f229e67077c54c0a44d
lephuquan <80085752+lephuquan@users.noreply.github.com>
2022-11-19 10:45:01 +0700

    update: updatePlace, updateUser

--- src/main/java/com/study/GreenPlace/service/PlaceService.java
+++ src/main/java/com/study/GreenPlace/service/PlaceService.java
@@ -161,20 +161,23 @@ public class PlaceService {
         places.setStar(placeModel.getStar());
         places.setRoad(placeModel.getRoad());
         places.setPhone(placeModel.getPhone());
-        places.setBrowserday(placeModel.getBrowserday());
+        places.setBrowserday(null);
         places.setPlacetypeid(placeTypeRepository.findById(placeModel.getPlacetypeid().getPlacetypeid()).get());
-        places.setUserid(userRepository.findById(placeModel.getUserid().getUserid()).get());
+        places.setUserid(userRepository.findById(placeModel.getUserid().getUserid()).get());//. userId have to exist in database
         places.setRatingsCollection(null);
-    places = placeRepository.save(places);
+        places.setImagesCollection(null);
+        places.setCommentsCollection(null);
+        places.setWishListItemsCollection(null);
+        places = placeRepository.save(places);
 
-    //handle list image
-    Collection<ImageModel> imageModels = placeModel.getImagesCollection();
+        //handle list image
+        Collection<ImageModel> imageModels = placeModel.getImagesCollection();
         for (ImageModel item: imageModels){
-        Images images = modelMapper.map(item, Images.class);
-        images.setPlaceid(places);
-        images = imageRepository.save(images);
-    }
-    //handle comment list
+            Images images = modelMapper.map(item, Images.class);
+            images.setPlaceid(places);
+            images = imageRepository.save(images);
+        }
+        //handle comment list
 //        Collection<CommentsModel> commentsModels = placeModel.getCommentsModels();
 //        for(CommentsModel item: commentsModels){
 //            Comments comments = modelMapper.map(item, Comments.class);
@@ -183,37 +186,33 @@ public class PlaceService {
 //            comments =  commentRepository.save(comments);
 //        }
 
-
-    //handle Rating list
+        //handle Rating list
 //        Collection<RatingsModel> ratingsModels = placeModel.getRatingsModelCollection();// rating dùng để lưu  đánh giá và là submit nhưng tiêu chỉ mà địa điểm này có
 //        for(RatingsModel item: ratingsModels){
 //            Ratings ratings = modelMapper.map(item, Ratings.class);
 //            ratings =  ratingRepository.save(ratings);
 //        }
 
-
-    //handle wishLishItem list
+        //handle wishLishItem list
 //        Collection<WishListItemsModel> wishListItemsModels = placeModel.getWishListItemsModels();
 //        for(WishListItemsModel item: wishListItemsModels){
 //            WishListItems wishListItems = modelMapper.map(item, WishListItems.class);
 //            wishListItems =  wishListItemsRepository.save(wishListItems);
 //        }
 
-
-//    Collection<RatingsModel> ratingsModels = placeModel.getRatingsCollection();// rating dùng để lưu  đánh giá và là submit nhưng tiêu chỉ mà địa điểm này có
-//        for(RatingsModel item: ratingsModels){
-//        List<Criterias> criteriasList = criteriaRepository.getListCriteriaByPlaceTypeId(places.getPlacetypeid().getPlacetypeid());
-//        for(Criterias criterias: criteriasList) {
-//            Ratings ratings = modelMapper.map(item, Ratings.class);
-//            ratings.setPlaceid(places);
-//            ratings.setUseridfr(places.getUserid());
-//            ratings.setCriteriaid(criterias);
-//            ratings =  ratingRepository.save(ratings);
-//        }
-//    }
-    //Adding a location does not need to add comments, ratings and wishlist
-    // because this information must be added to its own table. When adding place,
-    // only place information is added, not place's relational tables
+        Collection<RatingsModel> ratingsModels = placeModel.getRatingsCollection();// rating dùng để lưu  đánh giá và là submit nhưng tiêu chỉ mà địa điểm này có
+        for(RatingsModel item: ratingsModels){
+            // List<Criterias> criteriasList = criteriaRepository.getListCriteriaByPlaceTypeId(places.getPlacetypeid().getPlacetypeid());
+            Criterias criterias = criteriaRepository.findById(item.getCriteriasModel().getCriteriaid()).get();
+            Ratings ratings = modelMapper.map(item, Ratings.class);
+            ratings.setPlaceid(places);
+            ratings.setUseridfr(places.getUserid());// user rating, not user's place
+            ratings.setCriteriaid(criterias);
+            ratings =  ratingRepository.save(ratings);
+        }
+        //Adding a location does not need to add comments, ratings and wishlist
+        // because this information must be added to its own table. When adding place,
+        // only place information is added, not place's relational tables
         return "success";
 }
 
